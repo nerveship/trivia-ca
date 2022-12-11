f80e244035c98354a517fccdc2f3a030a1822e1c
Johnny <johnstuartgonsales@gmail.com>
2022-12-11 17:30:06 +0000

    Basic idea for loop implemented

--- Program.cs
+++ Program.cs
@@ -4,13 +4,20 @@ using System.IO.Enumeration;
 
 class Questions
 {
-    static void Main()
+    public static void Main()
     {
-        string[] questions = System.IO.File.ReadAllLines(@"F:\Creative Projects\programming\C#\questions\question\txts\questions.txt");
-        string[] answers = System.IO.File.ReadAllLines(@"F:\Creative Projects\programming\C#\questions\question\txts\answers.txt");
-
         Console.WriteLine("Welcome to my console trivia application!\nHow many questions would you like today?");
         int usrChoice = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(usrChoice);
+        Console.WriteLine($"Brilliant! Here come {usrChoice} questions!");
+
+        for (int i = 0; i < usrChoice; i++)
+        {
+            string[] questions = System.IO.File.ReadAllLines(@"F:\Creative Projects\programming\C#\questions\question\txts\questions.txt");
+            string[] answers = System.IO.File.ReadAllLines(@"F:\Creative Projects\programming\C#\questions\question\txts\answers.txt");
+
+            Random rand = new Random();
+            int index = rand.Next(questions.Length);
+            Console.WriteLine($"{questions[index]}");
+        }
     }
 }
