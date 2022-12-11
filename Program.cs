4c885199b8ef9eaf237769c862ca4d887d873f5e
Johnny <johnstuartgonsales@gmail.com>
2022-12-11 01:18:49 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+class Questions
+{
+    static void Main()
+    {
+        string[] sportQ = System.IO.File.ReadAllLines(@"F:\Creative Projects\programming\C#\questions\question\txts\sportQ.txt");
+        foreach (string line in sportQ)
+        {
+            Console.WriteLine(line);
+        }
+    }
+}
