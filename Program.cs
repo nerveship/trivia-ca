b42f21d243f076a64627a4dc9c3964f41483fce4
Johnny <johnstuartgonsales@gmail.com>
2022-12-11 17:32:59 +0000

    added a pause before continuing output of questions

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ class Questions
     {
         Console.WriteLine("Welcome to my console trivia application!\nHow many questions would you like today?");
         int usrChoice = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Brilliant! Here come {usrChoice} questions!");
+        Console.WriteLine($"Brilliant! Here come {usrChoice} questions!\n");
+        Thread.Sleep(2000);
 
         for (int i = 0; i < usrChoice; i++)
         {
@@ -17,6 +18,7 @@ class Questions
 
             Random rand = new Random();
             int index = rand.Next(questions.Length);
+            int ansrInput = 0;
             Console.WriteLine($"{questions[index]}");
         }
     }
