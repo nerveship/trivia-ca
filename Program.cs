5b4b257043df4e2e8cd606fe1b389217dee64249
Johnny <johnstuartgonsales@gmail.com>
2022-12-12 06:56:31 +0000

    changed to while loop

--- Program.cs
+++ Program.cs
@@ -16,16 +16,18 @@ class Questions
         Thread.Sleep(2000);
 
         //Loops over the amount of questions the user selected, starting with one and printing another when they answer
-        for (int i = 0; i < usrChoice; i++)
+        int i = 0;
+        while (i < usrChoice)
         {
             string[] qu = { "Is Scotland part of the UK?", "Does WWE stand for World Wrestling Education?" };
             string[] an = { "yes", "no" };
 
             Random rand = new();
             int index = rand.Next(qu.Length);
-          
+
             Console.WriteLine($"{qu[index]}");
-            string usrGuess =  Console.ReadLine();
+            string usrGuess = Console.ReadLine();
+            i++;
         }
     }
 }
