6c76f50114f34b68773bacc4c231ab21816388c1
Johnny <johnstuartgonsales@gmail.com>
2022-12-12 06:13:45 +0000

    added comments

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ class Questions
 {
     public static void Main()
     {
+        //Greets the user and prompts them for the number of questions they would like to answer
         Console.WriteLine("Welcome to my console trivia application!\nHow many questions would you like today?");
         int usrChoice = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine($"Brilliant! Here come {usrChoice} questions!\n");
+
+        //Waits before printing the first question
         Thread.Sleep(2000);
 
+        //Loops over the amount of questions the user selected, starting with one and printing another when they answer
         for (int i = 0; i < usrChoice; i++)
         {
             string[] qu = { "Is Scotland part of the UK?", "Does WWE stand for World Wrestling Education?" };
