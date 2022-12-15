189f45d53e23f8b6fcabcac5b94d3efa8766f847
Johnny <johnstuartgonsales@gmail.com>
2022-12-15 18:53:05 +0000

    added answer checking functionality

--- Program.cs
+++ Program.cs
@@ -2,32 +2,43 @@
 using System.Formats.Asn1;
 using System.IO;
 using System.IO.Enumeration;
+using System.Threading.Tasks.Sources;
 
 class Questions
 {
     public static void Main()
     {
+        int score = 0;
+        int maxQuestions = 3;
+
         //Greets the user and prompts them for the number of questions they would like to answer
         Console.WriteLine("Welcome to my console trivia application!\nHow many questions would you like today?");
         int usrChoice = Convert.ToInt32(Console.ReadLine());
+
+        while (usrChoice > maxQuestions)
+        {
+            Console.WriteLine($"Please choose between 1-{maxQuestions}");
+            usrChoice = Convert.ToInt32(Console.ReadLine());
+        }
         Console.WriteLine($"Brilliant! Here come {usrChoice} questions!\n");
 
         //Waits before printing the first question
         Thread.Sleep(2000);
 
         //Loops over the amount of questions the user selected, starting with one and printing another when they answer
-        int i = 0;
-        while (i < usrChoice)
+        for (int i = 0; i < usrChoice; i++)
         {
-            string[] qu = { "Is Scotland part of the UK?", "Does WWE stand for World Wrestling Education?" };
-            string[] an = { "yes", "no" };
+            string[] qu = { "Is Scotland part of the UK?", "Does WWE stand for World Wrestling Education?",
+            "Are there 5 countries in the UK?"};
+            string[] an = { "yes", "no", "no" };
 
-            Random rand = new();
-            int index = rand.Next(qu.Length);
-
-            Console.WriteLine($"{qu[index]}");
+            Console.WriteLine(qu[i]);
             string usrGuess = Console.ReadLine();
-            i++;
+            if (usrGuess == an[i])
+            {
+                score++;
+            }
         }
+        Console.WriteLine($"You got {score} questions right! Well done!");
     }
 }
